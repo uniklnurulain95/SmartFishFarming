1713002e9400067f57815a21de6ce0df2e652998
DannyDing96 <59546154+DannyDing96@users.noreply.github.com>
2020-01-12 23:06:03 +0800

    Update TurbiditySensor.py

--- TurbiditySensor.py
+++ TurbiditySensor.py
@@ -2,19 +2,21 @@
 import time.sleep
 
 while True
-  
+
   totalVolatgeReading = 0
   for count in range(800):
-    currentVolatgeReading = float(anlogRead(rpiGpioSensorPinNum / 1023)*5)
+    currentVoltageReading = float(anlogRead(rpiGpioSensorPinNum / 1023)*5)
     totalVoltageReading = totalVoltageReading + currentVoltageReading
     
-   averageVoltageReading = totalVoltageReading/800
+  averageVoltageReading = totalVoltageReading/800
    
-   roundedUpAverageVoltageReading = round(averageVoltageReading)
+  roundedUpAverageVoltageReading = round(averageVoltageReading)
    
-   if roundedUpAverageVoltageReading < 2.5:
-     nephelometricTurbidityUnit = -(1120.4* square(roundAverageVoltageReading) + (5742.3* roundUpAverageVoltageReading - 4353.8)
-     
+  if roundedUpAverageVoltageReading < 2.5:
+    nephelometricTurbidityUnit = 3000
+  elif
+    nephelometricTurbidityUnit = -(1120.4* square(roundAverageVoltageReading) + (5742.3* roundUpAverageVoltageReading - 4353.8)
+                                   
    lcd.clear()
    lcd.setCursor(0,0)
    lcd.print(roundedUpAverageVoltageReadig, "V")
